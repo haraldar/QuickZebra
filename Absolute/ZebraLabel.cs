956a8f876d41646cacb27369ed7e65102318804a
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-03-17 17:21:19 +0100

    Now manages the creation of the zebra elements fully.

--- Absolute/ZebraLabel.cs
+++ Absolute/ZebraLabel.cs
@@ -26,7 +26,8 @@ namespace QuickZebra.Absolute
         /// <param name="width">The label width (in inches).</param>
         /// <param name="height">The label height (in inches).</param>
         /// <param name="padding">The inner padding (in inches).</param>
-        public ZebraLabel(int density = 8, char quality = 'G', int width = 4, int height = 6, int padding = 0)
+        public ZebraLabel(int density = 8, char quality = 'G', int width = 4,
+            int height = 6, int padding = 0)
         {
             _density = density;
             _quality = quality;
@@ -49,8 +50,57 @@ namespace QuickZebra.Absolute
         public void AddFields(List<IZebraField> fieldList)
             => fieldList.ForEach(field => fields.Add(field));
 
-        //public void SetFont(char font = 'A', int height = 30, int width = 30)
-        //    => AddField(new ZebraFont(font, height, width));
+        public void AddText(string text, int x = 0, int y = 0,
+            bool invertIfOverlap = false)
+            => fields.Add(new ZebraText(text) { X = x, Y = y,
+                invertOnOverlap=invertIfOverlap});
+
+        public void AddMultipleText(List<string> dataStrings, int x = 0, int y = 0,
+            int xIncrement = 0, int yIncrement = 40, bool invertIfOverlap = false)
+            => fields.AddRange(new ZebraText().FromList(dataStrings, x, y,
+                xIncrement, yIncrement, invertIfOverlap));
+
+        public void SetFont(char font = 'A', int height = 30, int width = 0)
+            => fields.Add(new ZebraFont(font) { Height = height, Width = width});
+
+        public void AddComment(string comment)
+            => fields.Add(new ZebraComment(comment));
+
+        public void DrawBox(int x = 0, int y = 0, int height = 1, int width = 1,
+            int thickness = 1, char color = 'B', int rounding = 0,
+            bool invertIfOverlap = false)
+            => fields.Add(new ZebraGraphicalBox(thickness, color, rounding)
+            { X = x, Y = y, Height = height, Width = width,
+                invertOnOverlap = invertIfOverlap });
+
+        public void SetPlaceHolder(string id)
+            => fields.Add(new ZebraPlaceHolder(id));
+
+        public void ConfigureBarcode(int width = 2, double widthRatio = 3.0, int height = 10)
+            => fields.Add(new ZebraBarcodeConfig(width, widthRatio, height));
+
+        public void DrawBarCode(string content, int x = 0, int y = 0, char? orientation = null,
+            int? height = null, char? line = null, char? lineAbove = null,
+            char? checkDigit = null, char? mode = null, bool invertIfOverlap = false)
+            => fields.Add(new ZebraBarcode(content, orientation, height, line, lineAbove,
+                checkDigit, mode) { X = x, Y = y, invertOnOverlap = invertIfOverlap});
+
+        /// <summary>
+        /// Replaces the placeholder with the matching id.
+        /// </summary>
+        /// <param name="id">The id to match.</param>
+        /// <param name="insertFields">The fields to insert.</param>
+        /// <returns>The modified list of all fields.</returns>
+        public List<IZebraField> ReplaceHolder(string id, List<IZebraField> insertFields)
+        {
+            int index = fields.FindIndex(f => f.GetId() == id);
+            if (index != -1)
+            {
+                fields.RemoveAt(index);
+                fields.InsertRange(index, insertFields);
+            }
+            return fields;
+        }
 
         /// <summary>
         /// Converts the format into a ZPL code string.
@@ -101,6 +151,7 @@ namespace QuickZebra.Absolute
                 responseStream.CopyTo(fileStream);
                 responseStream.Close();
                 fileStream.Close();
+                Console.WriteLine("hi");
             }
             catch (WebException e)
             {
