77545becd5431b296160808f76fd9225505a43f8
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-03-17 17:33:09 +0100

    Initial commit.
    Can be used as a placeholder for other zebra elements, if one chooses the approach from the first version of this library.

---
+++ Absolute/ZebraPlaceHolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickZebra.Absolute
+{
+    public class ZebraPlaceHolder : ZebraField, IZebraField
+    {
+        public ZebraPlaceHolder(string id)
+            => Id = id;
+
+        public string? GetId()
+            => Id;
+
+        string IZebraField.Zebrify()
+            => EncapsuleLine(ZebraLexicon.FD + "Placeholder: " + Id);
+    }
+}
