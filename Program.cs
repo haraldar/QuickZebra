4f01b74d807ef927dec3a3ba17d4e1192f22d8a9
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-03-17 17:34:35 +0100

    Now also displays the second approach.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,6 @@
-// This constellation of code creates the example piece on the labelary.com's Online Viewer.
+﻿// This constellation of code creates the example piece on the labelary.com's Online Viewer.
 
 using QuickZebra.Absolute;
-
-ZebraLabel label = new();
-
-List<IZebraField> fields = new()
-{
-    // Section 1: Sender Data
-    new ZebraFont(font: '0', height: 60),
-    new ZebraGraphicalBox(width: 100, height: 100, thickness: 100) { X = 50, Y = 50 },
-    new ZebraGraphicalBox(width: 100, height: 100, thickness: 100) { X = 75, Y = 75, invertOnOverlap = true },
-    new ZebraGraphicalBox(width: 40, height: 40, thickness: 40) { X = 93, Y = 93 },
-    new ZebraText("Intershipping, Inc.") { X = 220, Y = 50 },
-    new ZebraGraphicalBox(width: 700, height: 3, thickness: 3) { X = 50, Y = 250 },
-    // Section 2: Recipient Data
-    new ZebraFont(height: 15),
-    new ZebraGraphicalBox(width: 150, height: 150, thickness: 3) { X = 600, Y = 300 },
-    new ZebraText("Permit") { X = 638, Y = 340 },
-    new ZebraText("123456") { X = 638, Y = 390 },
-    new ZebraGraphicalBox(width: 700, height: 3, thickness: 3) { X = 50, Y = 500 },
-    // Section 3: Barcode
-    new ZebraBarcodeConfig(width: 5, height: 270),
-    new ZebraBarcode("12345678") { X = 100, Y = 550 },
-    // Section 4: Final Data
-    new ZebraGraphicalBox(width: 700, height: 250, thickness: 3) { X = 50, Y = 900 },
-    new ZebraGraphicalBox(width: 3, height: 250, thickness: 3) { X = 400, Y = 900 },
-    new ZebraFont(font: '0', height: 190),
-    new ZebraText("CA") { X = 470, Y = 955 }
-};
 
 List<string> senderData = new()
 {
@@ -51,12 +24,105 @@ List<string> miscData = new()
     "REF2 BL4H8"
 };
 
-label.AddFields(fields);
-label.AddField(new ZebraFont(font: '0'));
-label.AddFields(new ZebraText() { X = 220, Y = 115 }.ConvertMultilineText(senderData));
-label.AddField(new ZebraFont());
-label.AddFields(new ZebraText() { X = 50, Y = 300}.ConvertMultilineText(receiverData));
-label.AddField(new ZebraFont(font:'0', height: 40));
-label.AddFields(new ZebraText() { X = 100, Y = 960}.ConvertMultilineText(miscData, yIncrement:50));
+// VERSION 2: Absolute, hidden object management
+ZebraLabel labelV2 = new();
 
-Console.WriteLine(label.GetLabelString(wrap: true, newlined: true));
+// Section 1: Sender Data
+labelV2.SetFont(font:'0', height: 60);
+labelV2.DrawBox(x: 50, y: 50, width: 100, height: 100, thickness: 100);
+labelV2.DrawBox(x: 75, y: 75, width: 100, height: 100, thickness: 100,
+    invertIfOverlap: true);
+labelV2.DrawBox(x: 93, y: 93, width: 40, height: 40, thickness: 40);
+labelV2.AddText("Intershipping, Inc.");
+labelV2.SetFont('0');
+labelV2.AddMultipleText(senderData, x: 220, y: 115);
+labelV2.DrawBox(x: 50, y: 250, width: 700, height: 3, thickness: 3);
+
+// Section 2: Recipient Data
+labelV2.DrawBox(x: 600, y: 300, width: 150, height: 150, thickness: 3);
+labelV2.SetFont();
+labelV2.AddMultipleText(receiverData, x:50, y:300);
+labelV2.SetFont(height: 15);
+labelV2.AddText("Permit", x: 638, y: 340);
+labelV2.AddText("123456", x: 638, y: 390);
+labelV2.DrawBox(x: 50, y: 500, width: 700, height: 3, thickness: 3);
+
+// Section 3: Barcode
+labelV2.ConfigureBarcode(width: 5, height: 270);
+labelV2.DrawBarCode("12345678", x: 100, y: 550);
+
+// Section 4: Final Data
+labelV2.DrawBox(x: 50, y: 900, width: 700, height: 250, thickness: 3);
+labelV2.SetFont(font: '0', height: 40);
+labelV2.AddMultipleText(miscData, x: 100, y: 960, yIncrement: 50);
+labelV2.DrawBox(x: 400, y: 900, width: 3, height: 250, thickness: 3);
+labelV2.SetFont('0', height: 190);
+labelV2.AddText("CA", x: 470, y: 955);
+
+Console.WriteLine(labelV2.GetLabelString(wrap: true, newlined: true));
+labelV2.CallLabelary(labelV2.GetLabelString(wrap: true));
+
+
+// VERSION 1: Absolute, raw object management
+List<IZebraField> fields = new()
+{
+    // Section 1: Sender Data
+    new ZebraFont('0')
+        { Height = 60 },
+    new ZebraGraphicalBox(thickness: 100)
+        { X = 50, Y = 50, Width = 100, Height = 100 },
+    new ZebraGraphicalBox(thickness: 100)
+        { X = 75, Y = 75, Width = 100, Height = 100, invertOnOverlap = true },
+    new ZebraGraphicalBox(thickness: 40)
+        { X = 93, Y = 93, Width = 40, Height = 40 },
+    new ZebraText("Intershipping, Inc.")
+        { X = 220, Y = 50 },
+    new ZebraFont('0'),
+    new ZebraPlaceHolder("sender"),
+    new ZebraGraphicalBox(thickness: 3)
+        { X = 50, Y = 250, Width = 700, Height = 3 },
+
+    // Section 2: Recipient Data
+    new ZebraGraphicalBox(thickness: 3)
+        { X = 600, Y = 300, Width = 150, Height = 150 },
+    new ZebraFont(),
+    new ZebraPlaceHolder("recipient"),
+    new ZebraFont()
+        { Height = 15 },
+    new ZebraText("Permit")
+        { X = 638, Y = 340 },
+    new ZebraText("123456")
+        { X = 638, Y = 390 },
+    new ZebraGraphicalBox(thickness: 3)
+        { X = 50, Y = 500, Width = 700, Height = 3 },
+
+    // Section 3: Barcode
+    new ZebraBarcodeConfig(width: 5, height: 270),
+    new ZebraBarcode("12345678")
+        { X = 100, Y = 550 },
+
+    // Section 4: Final Data
+    new ZebraGraphicalBox(thickness: 3)
+        { X = 50, Y = 900, Width = 700, Height = 250 },
+    new ZebraFont('0')
+        { Height = 40 },
+    new ZebraPlaceHolder("misc"),
+    new ZebraGraphicalBox(thickness: 3)
+        { X = 400, Y = 900, Width = 3, Height = 250 },
+    new ZebraFont(font: '0')
+        { Height = 190 },
+    new ZebraText("CA")
+        { X = 470, Y = 955 }
+};
+var labelV1 = new ZebraLabel();
+labelV1.AddFields(fields);
+
+var senderFields = new ZebraText().FromList(senderData, x: 220, y: 115);
+var recipientFields = new ZebraText().FromList(receiverData, x: 50, y: 300);
+var miscFields = new ZebraText().FromList(miscData, x: 100, y: 960, yIncrement: 50);
+labelV1.ReplaceHolder("sender", senderFields);
+labelV1.ReplaceHolder("recipient", recipientFields);
+labelV1.ReplaceHolder("misc", miscFields);
+
+//Console.WriteLine(labelV1.GetLabelString(wrap: true, newlined: true));
+//labelV1.CallLabelary(labelV1.GetLabelString(wrap: true));
