27fadbceea2d9e6671027e18ff562d8ae61d7f26
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-04-29 18:44:43 +0200

    A small unfinished amount of changes.

--- Program.cs
+++ Program.cs
@@ -24,45 +24,85 @@ List<string> miscData = new()
     "REF2 BL4H8"
 };
 
-// VERSION 2: Absolute, hidden object management
-ZebraLabel labelV2 = new();
+// VERSION 3: Small, but effective changes to V2 using tuples and TODO for relative, from this i might use sections!
+ZebraLabel labelV3 = new();
 
 // Section 1: Sender Data
-labelV2.SetFont(font:'0', height: 60);
-labelV2.DrawBox(x: 50, y: 50, width: 100, height: 100, thickness: 100);
-labelV2.DrawBox(x: 75, y: 75, width: 100, height: 100, thickness: 100,
-    invertIfOverlap: true);
-labelV2.DrawBox(x: 93, y: 93, width: 40, height: 40, thickness: 40);
-labelV2.AddText("Intershipping, Inc.");
-labelV2.SetFont('0');
-labelV2.AddMultipleText(senderData, x: 220, y: 115);
-labelV2.DrawBox(x: 50, y: 250, width: 700, height: 3, thickness: 3);
+labelV3.SetFont(font: '0', dims: (60, null));
+labelV3.DrawBox(loc: (50, 50), dims: (100, 100, 100));
+labelV3.DrawBox(loc: (75, 75), dims: (100, 100, 100), invertIfOverlap: true);
+labelV3.DrawBox(loc: (93, 93), dims: (40, 40, 40));
+labelV3.AddText("Intershipping, Inc.", loc: (220, 50));
+labelV3.SetFont(font: '0') ;
+labelV3.AddMultipleText(senderData, loc: (220, 115), incr: (null, null));
+labelV3.DrawBox(loc: (50, 250), dims: (700, 3, 3));
 
 // Section 2: Recipient Data
-labelV2.DrawBox(x: 600, y: 300, width: 150, height: 150, thickness: 3);
-labelV2.SetFont();
-labelV2.AddMultipleText(receiverData, x:50, y:300);
-labelV2.SetFont(height: 15);
-labelV2.AddText("Permit", x: 638, y: 340);
-labelV2.AddText("123456", x: 638, y: 390);
-labelV2.DrawBox(x: 50, y: 500, width: 700, height: 3, thickness: 3);
+labelV3.DrawBox(loc: (600, 300), dims: (150, 150, 3));
+labelV3.SetFont();
+labelV3.AddMultipleText(receiverData, loc: (50, 300), incr: (null, null));
+labelV3.SetFont(dims: (15, null));
+labelV3.AddText("Permit", loc: (638, 340));
+labelV3.AddText("123456", loc: (638, 390));
+labelV3.DrawBox(loc: (50, 500), dims: (700, 3, 3));
 
 // Section 3: Barcode
-labelV2.ConfigureBarcode(width: 5, height: 270);
-labelV2.DrawBarCode("12345678", x: 100, y: 550);
+labelV3.ConfigureBarcode(width: 5, height: 270);
+labelV3.DrawBarCode("12345678", x: 100, y: 550);
 
 // Section 4: Final Data
-labelV2.DrawBox(x: 50, y: 900, width: 700, height: 250, thickness: 3);
-labelV2.SetFont(font: '0', height: 40);
-labelV2.AddMultipleText(miscData, x: 100, y: 960, yIncrement: 50);
-labelV2.DrawBox(x: 400, y: 900, width: 3, height: 250, thickness: 3);
-labelV2.SetFont('0', height: 190);
-labelV2.AddText("CA", x: 470, y: 955);
+labelV3.DrawBox(loc: (50, 900), dims: (700, 250, 3));
+labelV3.SetFont(font: '0', dims: (40, null));
+labelV3.AddMultipleText(miscData, loc: (100, 960), incr: (0, 50));
+labelV3.DrawBox(loc: (400, 900), dims: (3, 250, 3));
+labelV3.SetFont(font: '0', dims: (190, null));
+labelV3.AddText("CA", loc: (470, 955));
 
-Console.WriteLine(labelV2.GetLabelString(wrap: true, newlined: true));
-labelV2.CallLabelary(labelV2.GetLabelString(wrap: true));
+Console.WriteLine(labelV3.GetLabelString(newlined: true));
+labelV3.CallLabelary(labelV3.GetLabelString());
 
 
+//// DEPRECATED
+//// VERSION 2: Absolute, hidden object management
+//ZebraLabel labelV2 = new();
+
+//// Section 1: Sender Data
+//labelV2.SetFont(font:'0', height: 60);
+//labelV2.DrawBox(x: 50, y: 50, width: 100, height: 100, thickness: 100);
+//labelV2.DrawBox(x: 75, y: 75, width: 100, height: 100, thickness: 100,
+//    invertIfOverlap: true);
+//labelV2.DrawBox(x: 93, y: 93, width: 40, height: 40, thickness: 40);
+//labelV2.AddText("Intershipping, Inc.", x: 220, y: 50);
+//labelV2.SetFont('0');
+//labelV2.AddMultipleText(senderData, x: 220, y: 115);
+//labelV2.DrawBox(x: 50, y: 250, width: 700, height: 3, thickness: 3);
+
+//// Section 2: Recipient Data
+//labelV2.DrawBox(x: 600, y: 300, width: 150, height: 150, thickness: 3);
+//labelV2.SetFont();
+//labelV2.AddMultipleText(receiverData, x:50, y:300);
+//labelV2.SetFont(height: 15);
+//labelV2.AddText("Permit", x: 638, y: 340);
+//labelV2.AddText("123456", x: 638, y: 390);
+//labelV2.DrawBox(x: 50, y: 500, width: 700, height: 3, thickness: 3);
+
+//// Section 3: Barcode
+//labelV2.ConfigureBarcode(width: 5, height: 270);
+//labelV2.DrawBarCode("12345678", x: 100, y: 550);
+
+//// Section 4: Final Data
+//labelV2.DrawBox(x: 50, y: 900, width: 700, height: 250, thickness: 3);
+//labelV2.SetFont(font: '0', height: 40);
+//labelV2.AddMultipleText(miscData, x: 100, y: 960, yIncrement: 50);
+//labelV2.DrawBox(x: 400, y: 900, width: 3, height: 250, thickness: 3);
+//labelV2.SetFont('0', height: 190);
+//labelV2.AddText("CA", x: 470, y: 955);
+
+//Console.WriteLine(labelV2.GetLabelString(newlined: true));
+//labelV2.CallLabelary(labelV2.GetLabelString());
+
+
+// DEPRECATED
 // VERSION 1: Absolute, raw object management
 List<IZebraField> fields = new()
 {
