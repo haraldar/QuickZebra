c752958fc44746755ee329246d592e38105ee9ed
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-05-11 18:34:04 +0200

    Adapted to newest library changes.

--- Program.cs
+++ Program.cs
@@ -32,14 +32,14 @@ section1.SetFont(font: '0', height: 60)
     .DrawBox(loc: (93, 93), dims: 40)
     .AddText("Intershipping, Inc.", loc: (220, 50))
     .SetFont(font: '0')
-    .AddMultipleText(senderData, loc: (220, 115), incr: (null, null))
+    .AddMultipleText(senderData, loc: (220, 115))
     .DrawBox(loc: (50, 250), dims: (700, 3, 3));
 
 // Section 2: Recipient Data
 ZebraLabel section2 = new();
 section2.DrawBox(loc: (600, 300), dims: (150, 150, 3))
     .SetFont()
-    .AddMultipleText(receiverData, loc: (50, 300), incr: (null, null))
+    .AddMultipleText(receiverData, loc: (50, 300))
     .SetFont(height: 15)
     .AddText("Permit", loc: (638, 340))
     .AddText("123456", loc: (638, 390))
@@ -54,13 +54,14 @@ section3.ConfigureBarcode(dims: (5, 270))
 ZebraLabel section4 = new();
 section4.DrawBox(loc: (50, 900), dims: (700, 250, 3))
     .SetFont(font: '0', height: 40)
-    .AddMultipleText(miscData, loc: (100, 960), incr: (0, 50))
+    .AddMultipleText(miscData, loc: (100, 960), down: 50)
     .DrawBox(loc: (400, 900), dims: (3, 250, 3))
     .SetFont(font: '0', height: 190)
     .AddText("CA", loc: (470, 955));
 
 // Build the full label.
 var sections = new List<ZebraLabel>() { section1, section2, section3, section4 };
-var labelV3 = new ZebraLabel().MergeLabels(sections);
+var labelV3 = new ZebraLabel(dims: (65, 100, true))
+    .MergeLabels(sections);
 Console.WriteLine(labelV3.GetLabelString(newlined: true));
-labelV3.CallLabelary(labelV3.GetLabelString());
+labelV3.CallLabelary(labelV3.GetLabelString(), labelFormat: "png");
