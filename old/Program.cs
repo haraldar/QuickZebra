bfe48a04a4d05f09fdebb3df13851bca4fb4cc4f
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-09-29 15:04:37 +0200

    Add files via upload

--- old/Program.cs
+++ old/Program.cs
@@ -1,27 +1,4 @@
-﻿// This constellation of code creates the example piece on the labelary.com's Online Viewer.
-
-using QuickZebra;
-List<string> senderData = new()
-{
-    "1000 Shipping Lane",
-    "Shelbyville TN 38102",
-    "United States (USA)"
-};
-
-List<string> receiverData = new()
-{
-    "John Doe",
-    "100 Main Street",
-    "Springfield TN 39021",
-    "United States (USA)"
-};
-
-List<string> miscData = new()
-{
-    "Ctr. X34B-1",
-    "REF1 F00B47",
-    "REF2 BL4H8"
-};
+﻿using QuickZebra;
 
 // Section 1: Sender Data
 var section1 = new ZebraLabel()
@@ -31,17 +8,25 @@ var section1 = new ZebraLabel()
     .DrawBox(loc: (93, 93), dims: 40)
     .AddText("Intershipping, Inc.", loc: (220, 50))
     .SetFont(font: '0')
-    .AddMultipleText(senderData, loc: (220, 115))
+    .AddTexts(loc: (220, 115), incr: (0, 40), invertIfOverlap: false,
+        "1000 Shipping Lane",
+        "Shelbyville TN 38102",
+        "United States (USA)")
     .DrawBox(loc: (50, 250), dims: (700, 3, 3));
 
 // Section 2: Recipient Data
 var section2 = new ZebraLabel()
     .DrawBox(loc: (600, 300), dims: (150, 150, 3))
     .SetFont()
-    .AddMultipleText(receiverData, loc: (50, 300))
+    .AddTexts(loc: (50, 300), incr: (0, 40), invertIfOverlap: false,
+        "John Doe",
+        "100 Main Street",
+        "Springfield TN 39021",
+        "United States (USA)")
     .SetFont(height: 15)
-    .AddText("Permit", loc: (638, 340))
-    .AddText("123456", loc: (638, 390))
+    .AddTexts(loc: (638, 340), incr: (0, 50), invertIfOverlap: false,
+        "Permit",
+        "123456")
     .DrawBox(loc: (50, 500), dims: (700, 3, 3));
 
 // Section 3: Barcode
@@ -52,14 +37,16 @@ var section3 = new ZebraLabel()
 var section4 = new ZebraLabel()
     .DrawBox(loc: (50, 900), dims: (700, 250, 3))
     .SetFont(font: '0', height: 40)
-    .AddMultipleText(miscData, loc: (100, 960), down: 50)
+    .AddTexts(loc: (100, 960), incr: (0, 50), invertIfOverlap: false,
+        "Ctr. X34B-1",
+        "REF1 F00B47",
+        "REF2 BL4H8")
     .DrawBox(loc: (400, 900), dims: (3, 250, 3))
     .SetFont(font: '0', height: 190)
     .AddText("CA", loc: (470, 955));
 
+
 //Build the full label.
-var sections = new List<ZebraLabel>() { section1, section2, section3, section4 };
-var labelV3 = new ZebraLabel(dims: (4, 6, false))
-    .MergeLabels(sections);
-Console.WriteLine(labelV3.GetLabelString(newlined: true));
-labelV3.CallLabelary(labelV3.GetLabelString(), labelFormat: "png");
+var labelV4 = new ZebraLabel(section1, section2, section3, section4);
+Console.WriteLine(labelV4.GetLabelString(newlined: true));
+labelV4.CallLabelary(labelV4.GetLabelString(), labelFormat: "png");
